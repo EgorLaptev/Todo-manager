aea93c214410b055ab2b80248af6627db219b5de
Egor Laptev <=>
2022-01-14 23:40:14 +0300

    Added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Auth from '../views/Auth.vue';
+import Component404 from '../views/Component404.vue';
 
 const routes = [
   { path: '/', component: Home, name: 'Home', alias: ['/home'] },
   { path: '/login', name: 'login', components: Auth },
+  { path: '/:pathMatch(.*)', component: Component404 }
 ];
 
 const router = createRouter({
