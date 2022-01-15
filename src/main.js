902b358409a3799b055550be194dd81cfd8e2bdc
Egor Laptev <=>
2022-01-15 15:40:06 +0300

    Added pwa

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
+import './registerServiceWorker'
 
 const app = createApp(App)
     .use(router)
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,18 @@ module.exports = {
       entry: 'src/main.js',
       title: 'Manage Todo',
     },
+  },
+  pwa: {
+    name: 'Todo manager',
+    themeColor: '#8ACAFE',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+  manifestOptions: {
+      display: 'standalone',
+      background_color: '#8ACAFE'
+  }
+
   }
 }
-
