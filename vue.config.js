3607305bd353a22c63ed2e82d9330aae9ea1c73c
Egor Laptev <=>
2022-01-20 21:03:57 +0300

    Modified vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,15 @@ module.exports = {
         display: 'standalone',
         background_color: '#FFFFFF'
     }
-  }
+  },
+  devServer: {
+    proxy: 'https://my.todos.gq'
+  },
+  outputDir: '../../../public/assets/app',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/assets/app/'
+    : '/',
+  indexPath: process.env.NODE_ENV === 'production'
+    ? '../../views/app.blade.php'
+    : 'index.html'
 }
